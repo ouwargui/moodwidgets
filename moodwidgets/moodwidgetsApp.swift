410794c37254a901a73541b98757510162b10072
Guilherme Santos <gui.santos@ibm.com>
2022-09-15 17:10:24 -0300

    Initial Commit

---
+++ moodwidgets/moodwidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodwidgetsApp.swift
+//  moodwidgets
+//
+//  Created by Guilherme Santos on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct moodwidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
